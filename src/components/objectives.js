45c0d088f972fe316fb0b06a12f6507deb69db7d
Josie Parker <josieparker@Josies-MacBook-Pro.local>
2024-05-09 17:48:51 -0500

    added objectives for proj

--- src/components/objectives.js
+++ src/components/objectives.js
@@ -1,6 +1,12 @@
 /*
 --- objectives for this project ---
+- an online store for purchasing bouquets and other plants and plant supplies!
 
-- 
-
+- create a functional online store. you should be able to:
+    - see a paginated list of products that can be filtered.
+    - be able to click on the product, see a product description, a price, a function to add that product to your basket.
+    - should see reviews on products, be able to add reviews of products, and see recommended products.
+    - have a persistent cart/basket with a price that automatically updates, as well as the can have items deleted from the cart.
+    - should be able to check out basket and take information like card number, address, email, stuff like that and send confirmation email to customer.
+    -obviously should have a responsive design.
 */
